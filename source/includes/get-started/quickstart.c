2fa51d3b781b47693fc1bba4c7157304cce1845d
Jordan Smith <jordan.smith@mongodb.com>
2024-09-16 12:47:47 -0700

    feedback

--- source/includes/get-started/quickstart.c
+++ source/includes/get-started/quickstart.c
@@ -4,35 +4,28 @@
 
 int main (int argc, char *argv[])
 {
-    mongoc_client_t *client;
-    mongoc_collection_t *collection;
-    mongoc_cursor_t *results;
     const bson_t *doc;
-    char *str;
 
     mongoc_init ();
 
-    client = mongoc_client_new ("<connection string>");
-    collection = mongoc_client_get_collection (client, "sample_mflix", "movies");
+    mongoc_client_t *client = mongoc_client_new ("<connection string>");
+    mongoc_collection_t *collection = mongoc_client_get_collection (client, "sample_mflix", "movies");
 
+    // Specify the query filter
+    bson_t *query = BCON_NEW ("title", "The Shawshank Redemption");
+
+    mongoc_cursor_t *results = mongoc_collection_find_with_opts (collection, query, NULL, NULL);
+
+    // Print the results
+    while (mongoc_cursor_next (results, &doc))
     {
-        // Specify the query filter
-        bson_t *query = BCON_NEW ("title", "The Shawshank Redemption");
-
-        results = mongoc_collection_find_with_opts (collection, query, NULL, NULL);
-
-        // Print the results
-        while (mongoc_cursor_next (results, &doc))
-        {
-            str = bson_as_canonical_extended_json (doc, NULL);
-            printf ("%s\n", str);
-            bson_free (str);
-        }
-
-        bson_free (query);
-        mongoc_cursor_destroy (results);
+        char* str = bson_as_canonical_extended_json (doc, NULL);
+        printf ("%s\n", str);
+        bson_free (str);
     }
 
+    bson_destroy (query);
+    mongoc_cursor_destroy (results);
     mongoc_collection_destroy (collection);
     mongoc_client_destroy (client);
     mongoc_cleanup ();
