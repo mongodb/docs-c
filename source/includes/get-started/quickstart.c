1ed4c3866d8bb2c97525e7e6d7d5d0d738e8c7b4
Jordan Smith <jordan.smith@mongodb.com>
2024-09-16 09:11:07 -0700

    update code

--- source/includes/get-started/quickstart.c
+++ source/includes/get-started/quickstart.c
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     mongoc_client_t *client;
     mongoc_collection_t *collection;
+    mongoc_cursor_t *results;
+    const bson_t *doc;
     char *str;
 
     mongoc_init();
@@ -15,18 +17,20 @@ int main(int argc, char *argv[])
 
     {
         // Specify the query filter
-        bson_t *query = bson_new()
-            BSON_APPEND_UTF8(query, "title", "The Shawshank Redemption");
+        bson_t *query = BCON_NEW("title", "The Shawshank Redemption");
 
-        mongoc_cursor_t results = mongoc_collection_find_with_opts(collection, query, NULL, NULL);
+        results = mongoc_collection_find_with_opts(collection, query, NULL, NULL);
 
         // Print the results
-        while (mongoc_cursor_next(cursor, &doc))
+        while (mongoc_cursor_next(results, &doc))
         {
             str = bson_as_canonical_extended_json(doc, NULL);
             printf("%s\n", str);
             bson_free(str);
         }
+
+        bson_free(query);
+        mongoc_cursor_destroy(results);
     }
 
     mongoc_collection_destroy(collection);
