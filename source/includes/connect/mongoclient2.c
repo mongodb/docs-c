b5f05a6dd4d4c70db549da9b626f2a102d3d01d2
Anika Malhotra <anika.malhotra@mongodb.com>
2024-09-23 15:43:07 -0400

    fix code snippets

--- source/includes/connect/mongoclient2.c
+++ source/includes/connect/mongoclient2.c
@@ -3,8 +3,8 @@
 
 int main(void) {
 
+  //start-connect-to-atlas
 	mongoc_client_t *client
-  mongoc_database_t *database
   mongoc_server_api_t *api = NULL;
 	bson_t *command, reply;
 	bson_error_t error;
@@ -15,7 +15,6 @@ int main(void) {
   // Intialize the MongoDB C Driver
 	mongoc_init();
 
-  //start-connect-to-atlas
   // Create a new client and connect to the server
   uri = "mongodb+srv://<db_username>:<db_password>@<hostname/port>/?<options>"
   client = mongoc_client_new (uri);
@@ -27,14 +26,14 @@ int main(void) {
     
   // Send a ping to confirm a successful connection
   command = BCON_NEW ("ping", BCON_INT32 (1));
-  retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
+  retval = mongoc_client_command_simple (database, command, NULL, &reply, &error);
 
   if (!retval) {
-	  fprintf (stderr, "%s\n", error.message);
-    return EXIT_FAILURE;
+    fprintf(stderr, "error: %s\n", error.message);
+    rc = 1;
+    goto cleanup;
   }
   printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
-  //end-connect-to-atlas
 
   // Cleanup
   bson_destroy (&reply);
@@ -44,4 +43,5 @@ int main(void) {
   mongoc_server_api_destroy(api);
   mongoc_client_destroy (client);
   mongoc_cleanup ();
+  //end-connect-to-atlas
 }
