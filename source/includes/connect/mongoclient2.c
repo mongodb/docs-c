8d5d509e150807125b71966dc4f506a2205fca4f
Anika Malhotra <anika.malhotra@mongodb.com>
2024-09-23 15:29:36 -0400

    update link and remove irrelevant content

--- source/includes/connect/mongoclient2.c
+++ source/includes/connect/mongoclient2.c
@@ -3,45 +3,45 @@
 
 int main(void) {
 
-		mongoc_client_t *client
-    mongoc_database_t *database
-    mongoc_server_api_t *api = NULL;
-		bson_t *command, reply;
-	  bson_error_t error;
-    char *uri
-	  bool retval;
-    bool ok = true;
+	mongoc_client_t *client
+  mongoc_database_t *database
+  mongoc_server_api_t *api = NULL;
+	bson_t *command, reply;
+	bson_error_t error;
+  char *uri
+	bool retval;
+  bool ok = true;
 
-    // Intialize the MongoDB C Driver
-		mongoc_init();
+  // Intialize the MongoDB C Driver
+	mongoc_init();
 
-    //start-connect-to-atlas
-    // Create a new client and connect to the server
-    uri = "mongodb+srv://<db_username>:<db_password>@<hostname/port>/?<options>"
-    client = mongoc_client_new (uri);
-		database = mongoc_client_get_database (client, "admin");
+  //start-connect-to-atlas
+  // Create a new client and connect to the server
+  uri = "mongodb+srv://<db_username>:<db_password>@<hostname/port>/?<options>"
+  client = mongoc_client_new (uri);
+	database = mongoc_client_get_database (client, "admin");
 
-    // Set the version of the Stable API on the client
-    api = mongoc_server_api_new(MONGOC_SERVER_API_V1);
-    ok = mongoc_client_set_server_api(client, api, &error);
+  // Set the version of the Stable API on the client
+  api = mongoc_server_api_new(MONGOC_SERVER_API_V1);
+  ok = mongoc_client_set_server_api(client, api, &error);
     
-    // Send a ping to confirm a successful connection
-    command = BCON_NEW ("ping", BCON_INT32 (1));
-    retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
+  // Send a ping to confirm a successful connection
+  command = BCON_NEW ("ping", BCON_INT32 (1));
+  retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
 
-    if (!retval) {
-	    fprintf (stderr, "%s\n", error.message);
-      return EXIT_FAILURE;
-    }
-    printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
-    //end-connect-to-atlas
+  if (!retval) {
+	  fprintf (stderr, "%s\n", error.message);
+    return EXIT_FAILURE;
+  }
+  printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
+  //end-connect-to-atlas
 
-    // Cleanup
-    bson_destroy (&reply);
-    bson_destroy (command);
-    bson_free (uri);
-    mongoc_database_destroy (database);
-    mongoc_server_api_destroy(api);
-    mongoc_client_destroy (client);
-    mongoc_cleanup ();
+  // Cleanup
+  bson_destroy (&reply);
+  bson_destroy (command);
+  bson_free (uri);
+  mongoc_database_destroy (database);
+  mongoc_server_api_destroy(api);
+  mongoc_client_destroy (client);
+  mongoc_cleanup ();
 }
