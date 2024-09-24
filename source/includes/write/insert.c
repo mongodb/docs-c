9fb9903be65658d345c9c221eac2c7904e93ce98
norareidy <nora.reidy@mongodb.com>
2024-09-24 16:25:19 -0400

    JS feedback

--- source/includes/write/insert.c
+++ source/includes/write/insert.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <bson/bson.h>
 #include <mongoc/mongoc.h>
 
 int
-main (int argc, char *argv[])
+main (void)
 {
     mongoc_client_t *client;
     mongoc_collection_t *collection;
     mongoc_init ();
 
-    client =
-        mongoc_client_new ("<connection string URI>");
+    client = mongoc_client_new ("<connection string URI>");
     collection = mongoc_client_get_collection (client, "sample_restaurants", "restaurants");
     
     {
@@ -30,25 +28,26 @@ main (int argc, char *argv[])
     {
         // Inserts documents representing restaurants into the collection
         // start-insert-many
-        bson_t *docs[2];
+        size_t num_docs = 2;
+	    bson_t *docs[num_docs];
         docs[0] = BCON_NEW ("name", BCON_UTF8 ("Mongo's Burgers"));
         docs[1] = BCON_NEW ("name", BCON_UTF8 ("Mongo's Pizza"));
 
         bson_error_t error;
-        if (!mongoc_collection_insert_many (collection, (const bson_t **)docs, 2, NULL, NULL, &error)) {
+        if (!mongoc_collection_insert_many (collection, (const bson_t **) docs, num_docs, NULL, NULL, &error)) {
             fprintf (stderr, "Insert many operation failed: %s\n", error.message);
         }
 
         bson_destroy (docs[0]);
         bson_destroy (docs[1]);
-        bson_destroy (&opts);
         // end-insert-many
     }
 
     {
         // Inserts multiple documents and instructs the insert operation to skip document-level validation
         // start-modify
-        bson_t *docs[3];
+        size_t num_docs = 3;
+	    bson_t *docs[num_docs];
         docs[0] = BCON_NEW ("name", BCON_UTF8("Mongo's Burgers"));
         docs[1] = BCON_NEW ("name", BCON_UTF8("Mongo's Pizza"));
         docs[2] = BCON_NEW ("name", BCON_UTF8("Mongo's Tacos"));
@@ -58,7 +57,7 @@ main (int argc, char *argv[])
         bson_append_bool (&opts, "bypassDocumentValidation", -1, true);
 
         bson_error_t error;
-        if (!mongoc_collection_insert_many (collection, (const bson_t **)docs, 3, &opts, NULL, &error)) {
+        if (!mongoc_collection_insert_many (collection, (const bson_t **) docs, num_docs, &opts, NULL, &error)) {
             fprintf (stderr, "Insert many operation failed: %s\n", error.message);
         }
 
