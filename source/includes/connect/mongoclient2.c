28224fab7ea1bcac68ef30aecc045276fc65bb63
Anika Malhotra <anika.malhotra@mongodb.com>
2024-09-24 09:34:51 -0400

    code cleanup

--- source/includes/connect/mongoclient2.c
+++ source/includes/connect/mongoclient2.c
@@ -19,7 +19,6 @@ int main(void) {
   // Create a new client and connect to the server
   uri = "mongodb+srv://<db_username>:<db_password>@<hostname/port>/?<options>";
   client = mongoc_client_new (uri);
-  // database = mongoc_client_get_database (client, "admin");
 
   // Set the version of the Stable API on the client
   api = mongoc_server_api_new(MONGOC_SERVER_API_V1);
