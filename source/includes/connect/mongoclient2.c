c3c7780e20bb0c3af7a304325d1603d5f05d7f3a
Anika Malhotra <anika.malhotra@mongodb.com>
2024-09-24 15:36:26 -0400

    add error check to stable api code block

--- source/includes/connect/mongoclient2.c
+++ source/includes/connect/mongoclient2.c
@@ -1,7 +1,7 @@
 #include <mongoc/mongoc.h>
 #include <bson/bson.h>
 
-int main (void) {
+int main(void) {
 
   //start-connect-to-atlas
   mongoc_client_t *client;
@@ -14,22 +14,28 @@ int main (void) {
   bool ok = true;
 
   // Intialize the MongoDB C Driver
-  mongoc_init ();
+  mongoc_init();
 
   // Create a new client and connect to the server
   uri = "mongodb+srv://<db_username>:<db_password>@<hostname/port>/?<options>";
   client = mongoc_client_new (uri);
+  // database = mongoc_client_get_database (client, "admin");
 
   // Set the version of the Stable API on the client
-  api = mongoc_server_api_new (MONGOC_SERVER_API_V1);
-  ok = mongoc_client_set_server_api (client, api, &error);
+  api = mongoc_server_api_new(MONGOC_SERVER_API_V1);
+  ok = mongoc_client_set_server_api(client, api, &error);
+  
+  if (!ok) {
+    fprintf(stderr, "error: %s\n", error.message);
+    return EXIT_FAILURE;
+  }
     
   // Send a ping to confirm a successful connection
   command = BCON_NEW ("ping", BCON_INT32 (1));
   retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
 
   if (!retval) {
-    fprintf (stderr, "error: %s\n", error.message);
+    fprintf(stderr, "error: %s\n", error.message);
     return EXIT_FAILURE;
   }
   printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
