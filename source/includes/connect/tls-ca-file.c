7f9717d9a279cc59b3eef1431e438f66f746a2b9
Anika Malhotra <anika.malhotra@mongodb.com>
2024-09-26 13:32:47 -0400

    new fleshed out code example for ca file

--- source/includes/connect/tls-ca-file.c
+++ source/includes/connect/tls-ca-file.c
@@ -7,16 +7,15 @@ int main(void) {
   // Intialize the MongoDB C Driver
   mongoc_init ();
   
-  // Create a new client and set TLS option to true
+  // Create a new client, set TLS option to true, set path to client certificate file
   mongoc_uri_t *uri = mongoc_uri_new ("mongodb://localhost:27017");  
   mongoc_uri_set_option_as_bool (uri, MONGOC_URI_TLS, true);
-
-  mongoc_client_t *client = mongoc_client_new_from_uri (uri);
-
+  mongoc_uri_set_option_as_utf8 (uri, MONGOC_URI_TLSCERTIFICATEKEYFILE, "/path/to/client-certificate.pem");  
+  
   // { Do database work here }
   
-  cleanup:
-    mongoc_client_destroy (client);
-    mongoc_cleanup ();
+  // Cleanup
+  mongoc_client_destroy (client);
+  mongoc_cleanup ();
   // end-connect
 }
