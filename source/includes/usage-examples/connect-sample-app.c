e43f7375efe30afda404673f1a93239b98ee7093
Anika Malhotra <anika.malhotra@mongodb.com>
2024-10-01 10:11:23 -0400

    sample app code changes

--- source/includes/usage-examples/connect-sample-app.c
+++ source/includes/usage-examples/connect-sample-app.c
@@ -3,26 +3,27 @@
 
 int main(void) {
 
-    mongoc_database_t *database
-		bson_t *ping, reply;
-	  bson_error_t error;
+  mongoc_database_t *database
+	bson_t *ping, reply;
+	bson_error_t error;
 
-		mongoc_init();
+	mongoc_init();
 
-    // Start example code here
+  // Start example code here
 
-    // End example code here
+  // End example code here
 
-		database = mongoc_client_get_database (client, "admin");
+	database = mongoc_client_get_database (client, "admin");
     
-    ping = BCON_NEW ("ping", BCON_INT32 (1));
+  ping = BCON_NEW ("ping", BCON_INT32 (1));
    
-    if (!mongoc_client_command_simple (client, "admin", ping, NULL, &reply, &error)) {
-	    fprintf (stderr, "%s\n", error.message);
-      return EXIT_FAILURE;
-    }
-    printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
+  if (!mongoc_client_command_simple (client, "admin", ping, NULL, &reply, &error)) {
+	  fprintf (stderr, "%s\n", error.message);
+    goto cleanup;
+  }
+  printf ("Pinged your deployment. You successfully connected to MongoDB!\n");
 
+  cleanup:
     bson_destroy (&reply);
     bson_destroy (ping);
     mongoc_database_destroy (database);
