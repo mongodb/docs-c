e0948b96b18cd79355b7e09f250b1c27d88b3227
Michael Morisi <michael.morisi@mongodb.com>
2024-10-01 11:52:01 -0400

    RR feedback

--- source/includes/write/transactions.c
+++ source/includes/write/transactions.c
@@ -1,73 +1,93 @@
 #include <bson/bson.h>
 #include <mongoc/mongoc.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// start-callback
+bool
+transaction_callback (mongoc_client_session_t *session)
+{
+    bson_error_t error;
+
+    mongoc_client_t *client = mongoc_client_session_get_client (session);
+
+    mongoc_collection_t *checking = mongoc_client_get_collection (client, "sample_bank", "checking");
+    mongoc_collection_t *savings = mongoc_client_get_collection (client, "sample_bank", "savings");
+    mongoc_collection_t *receipts = mongoc_client_get_collection (client, "sample_bank", "receipts");
+
+    char *account_id = "123456";
+    int transfer_amount = 1000;
+
+    bson_t *filter = BCON_NEW ("account_id", BCON_UTF8 (account_id));
+    bson_t *update_decrement = BCON_NEW ("$inc", "{", "balance", BCON_INT32 (-transfer_amount), "}");
+    bson_t *update_increment = BCON_NEW ("$inc", "{", "balance", BCON_INT32 (transfer_amount), "}");
+    
+    if (!mongoc_collection_update_one (checking, filter, update_decrement, NULL, NULL, &error)) {
+        fprintf (stderr, "Failed to update checking account: %s\n", error.message);
+        return false;
+    }
+
+    if (!mongoc_collection_update_one (savings, filter, update_increment, NULL, NULL, &error)) {
+        fprintf (stderr, "Failed to update savings account: %s\n", error.message);
+        return false;
+    }
+
+    bson_t *receipt = BCON_NEW ("account_id", BCON_UTF8 (account_id),
+                                "amount", BCON_INT32 (transfer_amount),
+                                "timestamp", BCON_DATE_TIME (bson_get_monotonic_time ()));
+    
+    if (!mongoc_collection_insert_one (receipts, receipt, NULL, NULL, &error)) {
+        fprintf (stderr, "Failed to insert receipt: %s\n", error.message);
+        return false;
+    }
+
+    mongoc_collection_destroy (checking);
+    mongoc_collection_destroy (savings);
+    mongoc_collection_destroy (receipts);
+
+    bson_destroy (filter);
+    bson_destroy (update_decrement);
+    bson_destroy (update_increment);
+    bson_destroy (receipt);
+
+    printf ("Transaction successful!");
+    return true;
+}
+// end-callback
 
 int
 main (int argc, char *argv[])
 {
     mongoc_client_t *client;
-    mongoc_collection_t *collection;
+    bson_error_t error;
 
     mongoc_init ();
 
     client =
         mongoc_client_new ("<connection string URI>");
-    collection =
-        mongoc_client_get_collection (client, "sample_restaurants", "restaurants");
 
-    {
-        // start-transaction
-        bson_error_t error;
-        mongoc_client_session_t *session = mongoc_client_start_session (client, NULL, &error);
-        if (!session) {
-            fprintf (stderr, "Could not start session: %s\n", error.message);
-            return EXIT_FAILURE;
-        }
+    // start-transaction
+    mongoc_client_session_t *session = mongoc_client_start_session (client, NULL, NULL);
+    if (!session) {
+        fprintf (stderr, "Failed to start session\n");
 
-        // Start the transaction
-        if (!mongoc_client_session_start_transaction (session, NULL, &error)) {
-            fprintf (stderr, "Could not start transaction: %s\n", error.message);
-            return EXIT_FAILURE;
-        }
-
-        // Create documents to insert
-        size_t num_docs = 2;
-        bson_t *docs[num_docs];
-        docs[0] = BCON_NEW ("name", BCON_UTF8 ("Mongo's Burgers"));
-        docs[1] = BCON_NEW ("name", BCON_UTF8 ("Mongo's Pizza"));
-
-        // Append the session to the insert options
-        bson_t *insert_opts = bson_new ();
-        if (!mongoc_client_session_append (session, insert_opts, &error)) {
-            fprintf (stderr, "Could not append session to opts: %s\n", error.message);
-            return EXIT_FAILURE;
-        }
-
-        // Insert the documents
-        if (!mongoc_collection_insert_many (collection, (const bson_t **) docs, num_docs, insert_opts, NULL, &error)) {
-            fprintf (stderr, "Insert many operation failed: %s\n", error.message);
-            mongoc_client_session_abort_transaction (session, &error);
-            return EXIT_FAILURE;
-        }
-
-        // Commit the transaction
-        if (!mongoc_client_session_commit_transaction (session, NULL, &error)) {
-            fprintf (stderr, "Failed to commit transaction: %s\n", error.message);
-            return EXIT_FAILURE;
-        }
-
-        // Cleanup
-        for (size_t i = 0; i < num_docs; i++) {
-            bson_destroy (docs[i]);
-        }
-        mongoc_client_session_destroy (session);
-        bson_destroy (insert_opts);
-        // end-transaction
+        mongoc_client_destroy (client);
+        return EXIT_FAILURE;
     }
 
-    mongoc_collection_destroy (collection);
+    bool result = 
+        mongoc_client_session_with_transaction (session,
+                                                (mongoc_client_session_with_transaction_cb_t) transaction_callback,
+                                                NULL, NULL, NULL, &error);
+
+    if (!result) {
+        fprintf (stderr, "Transaction error: %s\n", error.message);
+    }
+
+    mongoc_client_session_destroy (session);
     mongoc_client_destroy (client);
     mongoc_cleanup ();
+    // end-transaction
 
     return EXIT_SUCCESS;
 }
