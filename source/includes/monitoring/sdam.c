d7180a30d2ddca9e87a44776cf650eba9ce27f6c
norareidy <nora.reidy@mongodb.com>
2024-10-02 09:37:21 -0400

    EC feedback

--- source/includes/monitoring/sdam.c
+++ source/includes/monitoring/sdam.c
@@ -9,10 +9,8 @@ typedef struct {
 static void
 server_opening (const mongoc_apm_server_opening_t *event)
 {
-   stats_t *context;
-
-   context = (stats_t *) mongoc_apm_server_opening_get_context (event);
-   context->server_opening_events++;
+   stats_t *stats = (stats_t *) mongoc_apm_server_opening_get_context (event);
+   stats->server_opening_events += 1;
 
    printf ("Server opening: %s\n", mongoc_apm_server_opening_get_host (event)->host_and_port);
 }
@@ -20,21 +18,24 @@ server_opening (const mongoc_apm_server_opening_t *event)
 int
 main (void)
 {
-   mongoc_client_t *client;
-   mongoc_apm_callbacks_t *cbs
-   stats_t stats = {0};
    mongoc_init ();
 
-   client = mongoc_client_new ("<connection string URI>");
+   stats_t stats = {0};
 
-   cbs = mongoc_apm_callbacks_new ();
-   mongoc_apm_set_server_opening_cb (cbs, server_opening);
-   mongoc_client_set_apm_callbacks (client, cbs, (void *) &stats);
+   mongoc_client_t *client = mongoc_client_new ("<connection string URI>");
+   
+   {
+      mongoc_apm_callbacks_t *cbs = mongoc_apm_callbacks_new ();
+      mongoc_apm_set_server_opening_cb (cbs, server_opening);
+      mongoc_client_set_apm_callbacks (client, cbs, &stats);
+      mongoc_apm_callbacks_destroy (cbs);
+   }
 
    // Perform database operations
 
-   mongoc_client_destroy (client);
-   mongoc_apm_callbacks_destroy (cbs);
+   mongoc_client_destroy (client);   
+
+   printf ("Observed %d server opening events\n", stats.server_opening_events);
 
    mongoc_cleanup ();
 
