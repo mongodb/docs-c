6f6ef863c321d0069bbb47a8a81704b795997d26
Stephanie Aurelio <stephanie.aurelio@mongodb.com>
2024-10-03 10:16:41 -0700

    KA feedback

--- source/includes/usage-examples/run-command.c
+++ source/includes/usage-examples/run-command.c
@@ -7,8 +7,6 @@ main (int argc, char *argv[])
 {
     mongoc_client_t *client;
     mongoc_collection_t *collection;
-    char *str;
-    bson_error_t error;
 
     mongoc_init ();
 
@@ -18,17 +16,16 @@ main (int argc, char *argv[])
 
     {
         // start-execute-command
-        bson_t *command, reply;
+        bson_t reply;
         bson_error_t error;
-        bool retval;
     
-        command = BCON_NEW ("hello", BCON_INT32 (1));
-        retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
+        bson_t *command = BCON_NEW ("hello", BCON_INT32 (1));
+        bool retval = mongoc_client_command_simple (client, "admin", command, NULL, &reply, &error);
     
         if (!retval) {
             fprintf (stderr, "Failed to run command: %s\n", error.message);
         } else {
-         char *str = bson_as_canonical_extended_json (&reply, NULL);
+            char *str = bson_as_canonical_extended_json (&reply, NULL);
             printf ("%s\n", str);
             bson_free (str);
         }
@@ -40,24 +37,20 @@ main (int argc, char *argv[])
 
     {
         // start-execute-command-with-options
-        bson_t *command;
-        bson_t *opts;
         bson_t reply;
         bson_error_t error;
-        bool retval;
 
-        command = BCON_NEW(
+        bson_t *command = BCON_NEW(
             "grantRolesToUser", BCON_UTF8("user011"),
             "roles", "[", BCON_UTF8("readWrite"), "]"
         );
 
-        opts = BCON_NEW(
-            "writeConcern", "{",
-                "w", BCON_UTF8("majority"),
-            "}"
-        );
+        mongoc_write_concern_t *write_concern = mongoc_write_concern_new();
+        mongoc_write_concern_set_w (write concern, MONGOC_WRITE_CONCERN_W_MAJORITY);
+        bson_t *opts = bson_new();
+        mongoc_write_concern_append(write_concern, opts);
 
-        retval = mongoc_client_command_with_opts(client, "admin", command, opts, &reply, &error);
+        bool retval = mongoc_client_command_with_opts(client, "admin", command, NULL, opts, &reply, &error);
 
         if (!retval) {
             fprintf(stderr, "Failed to run command: %s\n", error.message);
@@ -69,22 +62,16 @@ main (int argc, char *argv[])
 
         bson_destroy(command);
         bson_destroy(opts);
-        bson_destroy(&reply)
+        bson_destroy(&reply);
         // end-execute-command-with-options
     }
 
     {
         // start-run-command-example
-        bson_t *cmd;
-        bson_t *opts;
-        mongoc_write_concern_t *write_concern;
-        mongoc_read_prefs_t *read_prefs;
-        mongoc_read_concern_t *read_concern;
         bson_t reply;
         bson_error_t error;
-        char *json;
 
-        cmd = BCON_NEW ("cloneCollectionAsCapped",
+        bson_t *cmd = BCON_NEW ("cloneCollectionAsCapped",
                 BCON_UTF8 ("my_collection"),
                 "toCollection",
                 BCON_UTF8 ("my_capped_collection"),
@@ -92,15 +79,15 @@ main (int argc, char *argv[])
                 BCON_INT64 (1024 * 1024));
 
         /* Includes write concern "majority" in command options */
-        write_concern = mongoc_write_concern_new ();
-        mongoc_write_concern_set_wmajority (write_concern, 10000 /* wtimeoutMS */);
-        opts = bson_new ();
+        mongo_write_concern_t *write_concern = mongoc_write_concern_new ();
+        mongoc_write_concern_set_w (write_concern, MONGOC_WRITE_CONCERN_W_MAJORITY);
+        bson_t *opts = bson_new ();
         mongoc_write_concern_append (write_concern, opts);
 
         if (mongoc_client_write_command_with_opts (client, "test", cmd, opts, &reply, &error)) {
-            json = bson_as_canonical_extended_json (&reply, NULL);
-            printf ("cloneCollectionAsCapped: %s\n", json);
-            bson_free (json);
+            char *str = bson_as_canonical_extended_json (&reply, NULL);
+            printf ("cloneCollectionAsCapped: %s\n", str);
+            bson_free (str);
         } else {
             fprintf (stderr, "cloneCollectionAsCapped: %s\n", error.message);
         }
@@ -122,20 +109,20 @@ main (int argc, char *argv[])
                 "}",
                 "}");
 
-        read_prefs = mongoc_read_prefs_new (MONGOC_READ_SECONDARY);
+        mongoc_read_prefs_t *read_prefs = mongoc_read_prefs_new (MONGOC_READ_SECONDARY);
 
         /* Specifies "One" sorts after "one" to override default behavior */
         opts = BCON_NEW ("collation", "{", "locale", BCON_UTF8 ("en_US"), "caseFirst", BCON_UTF8 ("lower"), "}");
 
         /* Adds a read concern to "opts" */
-        read_concern = mongoc_read_concern_new ();
+        mongoc_read_concern_t *read_concern = mongoc_read_concern_new ();
         mongoc_read_concern_set_level (read_concern, MONGOC_READ_CONCERN_LEVEL_MAJORITY);
         mongoc_read_concern_append (read_concern, opts);
 
         if (mongoc_client_read_command_with_opts (client, "test", cmd, read_prefs, opts, &reply, &error)) {
-            json = bson_as_canonical_extended_json (&reply, NULL);
-            printf ("distinct: %s\n", json);
-            bson_free (json);
+            str = bson_as_canonical_extended_json (&reply, NULL);
+            printf ("distinct: %s\n", str);
+            bson_free (str);
         } else {
             fprintf (stderr, "distinct: %s\n", error.message);
         }
